5dde67d07c4c8c5ab3d98a143684ec4f90c7e1d9
Leire L S <parecementeria@gmail.com>
2014-12-02 13:56:00 +0100

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name petitionsorgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the petitionsorgApp
+ */
+angular.module('petitionsorgApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
